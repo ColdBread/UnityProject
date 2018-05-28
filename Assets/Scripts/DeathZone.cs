455ac4a41b9487bb762ae6854f4f178c5fd7ae2e
ColdBread <sdimka0777@gmail.com>
2018-05-28 10:33:30 +0300

    HW_2

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        //Намагаємося отримати компонент кролика
+        HeroRabbit rabit = collider.GetComponent<HeroRabbit>();
+        //Впасти міг не тільки кролик
+        if (rabit != null)
+        {
+            //Повідомляємо рівень, про смерть кролика
+            LevelController.current.onRabitDeath(rabit);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+    public static LevelController current;
+    Vector3 startingPosition;
+
+    public void onRabitDeath(HeroRabbit rabit)
+    {
+        //При смерті кролика повертаємо на початкову позицію
+        rabit.transform.position = this.startingPosition;
+    }
+
+    public void setStartPosition(Vector3 pos)
+    {
+        this.startingPosition = pos;
+    }
+
+    void Awake()
+    {
+        current = this;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
